2b7920e231ddfba1425a797997eecf56c1a5f0df
Gabriel <95183526+ImGabriell@users.noreply.github.com>
2022-03-30 22:05:48 -0300

    Add files via upload

---
+++ aula04/atividade.java
@@ -0,0 +1,29 @@
+public class atividade //Onde ficam as classes.
+{
+    //public static void main(String[] args) 
+    //{
+        //for (int i = 1; i <= 10; i++) 
+        //{
+            //System.out.println("\nTabuada do " +i);
+            
+            //for (var j = 1; j <= 10; j++)
+            //{
+                //System.out.println(i+" X "+j+" = " +i*j);
+            //}
+        //}
+    //}
+
+    public static void main(String[] args) // Onde é codado.
+    {
+        for(int i = 1; i <= 10; i++)
+        {
+            for(int j = 1; j <= 10; j++)
+            {
+                System.out.printf("%d x %d = %d\n", i, j, i * j);
+            }
+        }    
+    }
+}
+
+
+
