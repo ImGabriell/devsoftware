912a66364846ec153c2209e6ac667ec9e9de4210
Gabriel <95183526+ImGabriell@users.noreply.github.com>
2022-05-11 22:10:01 -0300

    Add files via upload

---
+++ aula08/logica/FormaGeometrica.java
@@ -0,0 +1,7 @@
+package logica;
+public abstract class FormaGeometrica implements PreenchimentoFormas
+{
+
+    public abstract double calcularArea();
+    
+} 
---
+++ aula08/logica/ProgramaPrincipal.java
@@ -0,0 +1,12 @@
+package logica;
+
+import grafica.FrmCalculadoraGeometrica;
+
+public class ProgramaPrincipal 
+{
+    public static void main(String[] args) 
+    {
+        FrmCalculadoraGeometrica frm = new FrmCalculadoraGeometrica();
+        frm.setVisible(true);
+    }
+}
