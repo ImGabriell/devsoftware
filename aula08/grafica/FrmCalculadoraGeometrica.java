7e2017f99e79846975610b459228aeb71af40492
Gabriel <95183526+ImGabriell@users.noreply.github.com>
2022-05-18 21:30:55 -0300

    Add files via upload

--- aula08/grafica/FrmCalculadoraGeometrica.java
+++ aula08/grafica/FrmCalculadoraGeometrica.java
@@ -9,6 +9,8 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 
+import grafica.ActionCalcularArea.OpcaoForma;
+
 public class FrmCalculadoraGeometrica extends JFrame
 {
 
@@ -30,30 +32,61 @@ public class FrmCalculadoraGeometrica extends JFrame
         setSize(new Dimension(720, 200));
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLayout(new GridLayout(3, 1, 10, 10));
-        
+    
+        //Círculo
         JPanel pnl = new JPanel();
-
         pnl.setBorder(BorderFactory.createTitledBorder("Círculo"));
         add(pnl);
-        pnl.setLayout(new GridLayout(1, 5, 10, 50));
+        pnl.setLayout(new FlowLayout(FlowLayout.LEFT, 10, 10));
         pnl.add(new JLabel("Raio: "));
-
-        txtCircunferenciaRaio = new JTextField();
+        txtCircunferenciaRaio = new JTextField(10);
         pnl.add(txtCircunferenciaRaio);
-
         btnCircunferenciaCalcularArea = new JButton("Calcular");
         pnl.add(btnCircunferenciaCalcularArea);
         pnl.add(new JLabel("Área"));
-
-        txtCircunferenciaArea = new JTextField();
+        txtCircunferenciaArea = new JTextField(10);
         pnl.add(txtCircunferenciaArea);
 
+        //Retângulo
         pnl = new JPanel();
         pnl.setBorder(BorderFactory.createTitledBorder("Retângulo"));
         add(pnl);
+        pnl.add(new JLabel("Base"));
+        pnl.add(txtRetanguloBase = new JTextField(10));
+        pnl.add(new JLabel("Altura"));
+        pnl.add(txtRetanguloAltura = new JTextField(10));
+        pnl.add(btnRetanguloCalcularArea = new JButton("Calcular"));
+        pnl.add(new JLabel("Área:"));
+        pnl.add(txtRetanguloArea = new JTextField(10));
 
+        //Triângulo
         pnl = new JPanel();
         pnl.setBorder(BorderFactory.createTitledBorder("Triângulo"));
         add(pnl);
+        pnl.setLayout(new FlowLayout(FlowLayout.LEFT, 10, 10));
+        pnl.add(new JLabel("Base"));
+        pnl.add(txtTrianguloBase = new JTextField(10));
+        pnl.add(new JLabel("Altura"));
+        pnl.add(txtTrianguloAltura = new JTextField(10));
+        pnl.add(btnTrianguloCalcularArea = new JButton("Calcular"));
+        pnl.add(new JLabel("Área:"));
+        pnl.add(txtTrianguloArea = new JTextField(10));
+
+        pack();
+
+        ActionCalcularArea action = new ActionCalcularArea
+        (txtCircunferenciaRaio, OpcaoForma.CIRCULO, txtCircunferenciaArea);
+
+        btnCircunferenciaCalcularArea.addActionListener(action);
+
+        ActionCalcularArea action2 = new ActionCalcularArea
+        (txtRetanguloAltura, txtRetanguloBase, OpcaoForma.RETANGULO, txtRetanguloArea);
+
+        btnRetanguloCalcularArea.addActionListener(action2);
+
+        ActionCalcularArea action3 = new ActionCalcularArea
+        (txtTrianguloAltura, txtTrianguloBase, OpcaoForma.TRIANGULO, txtTrianguloArea);
+
+        btnTrianguloCalcularArea.addActionListener(action3);
     }
 }
--- aula08/logica/Triangulo.java
+++ aula08/logica/Triangulo.java
@@ -5,7 +5,7 @@ public class Triangulo extends FormaGeometrica
     private double base;
     private double altura;
 
-    public Triangulo()
+    public Triangulo(double base2)
     {
 
     }
